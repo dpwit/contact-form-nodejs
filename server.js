73dd766a619e71d825a17ae6fe1698ca5dbf75fd
darrenwhatford <darrenwhatford@hastingsdirect.com>
2021-04-01 17:31:34 +0100

    added form functionality

--- server.js
+++ server.js
@@ -1,22 +1,27 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const log = console.log;
 const app = express();
 const path = require('path');
 const PORT = 8080;
 
 // Configuring our data parsing
-app.use(express.urlencoded({
-    extend: false
+app.use(bodyParser.urlencoded({
+    extend: true
 }));
 app.use(express.static('views'));
 
 app.post('/email', (req, res) => {
     //Send an email here but currently dummy email
-    console.log('Data:', req.body);
-    res.json({message: 'Message received!'})
+    console.log('Name:', req.body.name);
+    console.log('Email:', req.body.email);
+    console.log('Subject:', req.body.subject);
+    console.log('Message:', req.body.message);
+    //res.json({message: 'Message received!'})
+    res.redirect('/thanks.html')
 });
 
-app.get('/', function(req, res) {
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'index.html'));
     //__dirname : It will resolve to your project folder.
 });
